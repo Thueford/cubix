3f559a845fabd79ff60175a9e8aff86133c84886
afeilke1 <afeilke1@hs-mittweida.de>
2021-06-09 22:17:31 +0200

    added GameStage props

--- Assets/Scripts/Controller/GameStage.cs
+++ Assets/Scripts/Controller/GameStage.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameStage : MonoBehaviour
 {
     public GameObject cam, spawn, actors, next;
+    public int colorSlots = 3;
+    public float chargingSpeed = 1;
 
     public void Start()
     {
@@ -32,7 +34,7 @@ public class GameStage : MonoBehaviour
 
         Hint h = GetComponentInChildren<Hint>();
         if (h != null) h.ResetHints();
-        GetComponentInChildren<ChargeAnim>().Reset();
+        GetComponentInChildren<ChargeAnim>().Reset(chargingSpeed);
         GetComponentInChildren<Spawn>().Disable();
         if(next != null) next.GetComponentInChildren<Spawn>().Enable();
 
