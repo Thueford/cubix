3d7b674f6dd40fc8d42d7d8c0c1c1ac5dd44a941
Jakob Maßalsky <jmassals@hs-mittweida.com>
2021-06-12 21:38:09 +0200

    Done with post processing, it was a long ride

--- Assets/Scripts/Shader/Compute.cs
+++ Assets/Scripts/Shader/Compute.cs
@@ -6,32 +6,71 @@ public class Compute : MonoBehaviour
 {
     public ComputeShader bloom, chromaticAberration, lensFlare;
 
-    public RenderTexture sourceTex, brightTex, blurrBuff, bloomResult, caResult;
+    public Material blurrMat;
+
+    public RenderTexture sourceTex,
+        brightTex,
+        blurrBuff,
+        caResult,
+        lfResult;
+
+    private RenderTexture lensDirt;
+    public Texture2D lensDirtTex;
+
+    public bool skipAll = false;
 
     public bool useBloom = true;
-    public bool useCA = true;
-
+    
     [Range(1, 20)]
-    public uint bloomBlurrAmount = 1;
+    public int bloomBlurrAmount = 1;
 
     [Range(0.01f, 1f)]
-    public float bloomThreshold = .5f;
+    public float bloomThreshold = .65f;
+
+    public bool useCA = true;
 
     [Range(1, 10)]
     public int caAmount = 3;
 
+    public bool useLensFlare = true;
+
+    [Range(1, 10)]
+    public int lfGhostCount = 3;
+
+    [Range(1, 20)]
+    public int lfBlurrCount = 1;
+
+    [Range(0.01f, 1f)]
+    public float lfThreshold = 0.7f;
+
+    [Range(0.1f, 2f)]
+    public float lfGhostSpacing = 1.4f;
+
+    [Range(0.0f, 0.2f)]
+    public float lfCAStrength = 0.1f;
+
+    public bool useVignette = true;
+
+    [Range(0.0f, 1f)]
+    public float vignetteAmount = 0.8f;
+
+    [Range(0.0f, 0.4f)]
+    public float vignetteWidth = 0.1f;
+
     private const int numThreads = 24;
 
-    private int xThreads = Screen.width / numThreads;
-    private int yThreads = Screen.width / numThreads;
+    private int xThreads = (int)Mathf.Ceil(Screen.width / numThreads);
+    private int yThreads = (int)Mathf.Ceil(Screen.height / numThreads);
+
+    private int lastWidth = Screen.width;
+    private int lastHeight = Screen.height;
 
     // Start is called before the first frame update
     void Start()
     {
-        sourceTex = createTexture();
-        brightTex = createTexture();
-        blurrBuff = createTexture();
-        caResult = createTexture();
+        createTextures();
+
+        setTextures();
 
         setBloomUniforms();
         setCAUniforms();
@@ -40,31 +79,75 @@ public class Compute : MonoBehaviour
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+        if (Screen.width != lastWidth ||Screen.height != lastHeight)
+        {
+            lastWidth = Screen.width;
+            lastHeight = Screen.height;
+            xThreads = (int)Mathf.Ceil(Screen.width / numThreads);
+            yThreads = (int)Mathf.Ceil(Screen.height / numThreads);
+
+            createTextures();
+            setTextures();
+        }
+
+        if (skipAll)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
         Graphics.Blit(source, sourceTex);
 
         if (useBloom)
         {
-            bloom.SetFloat("threshold", bloomThreshold);
+            setBloomUniforms();
+
             bloom.Dispatch(0, xThreads, yThreads, 1);
 
-            for (int i = 0; i < bloomBlurrAmount; i++)
-            {
-                bloom.Dispatch(1, xThreads, yThreads, 1);
-                bloom.Dispatch(2, xThreads, yThreads, 1);
-            }
+            blurr(brightTex, bloomBlurrAmount);
 
-            bloom.Dispatch(3, xThreads, yThreads, 1);
+            bloom.Dispatch(1, xThreads, yThreads, 1);
         }
 
         if (useCA)
         {
-            chromaticAberration.SetInt("amount", caAmount);
-            chromaticAberration.Dispatch(0, xThreads, yThreads, 1);
-            Graphics.Blit(caResult, destination);
-        }
-        else
-            Graphics.Blit(sourceTex, destination);
+            setCAUniforms();
 
+            Graphics.Blit(sourceTex, caResult);
+
+            chromaticAberration.Dispatch(0, xThreads, yThreads, 1);
+        } 
+        else Graphics.Blit(sourceTex, caResult);
+
+        if (useLensFlare)
+        {
+            setLensFlareUniforms();
+
+            lensFlare.Dispatch(0, xThreads, yThreads, 1);
+
+            blurr(lfResult, lfBlurrCount);
+
+            lensFlare.Dispatch(1, xThreads, yThreads, 1);
+        }
+        if (useVignette)
+        {
+            blurrMat.SetFloat("_vignetteAmount", vignetteAmount);
+            blurrMat.SetFloat("_vignetteWidth", vignetteWidth);
+            Graphics.Blit(caResult, destination, blurrMat, 1);
+        }
+        else Graphics.Blit(caResult, destination);
+
+    }
+
+    private void blurr(RenderTexture tex, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            blurrMat.SetInt("_horizontal", 1);
+            Graphics.Blit(tex, blurrBuff, blurrMat, 0);
+            blurrMat.SetInt("_horizontal", 0);
+            Graphics.Blit(blurrBuff, tex, blurrMat, 0);
+        }
     }
 
     private RenderTexture createTexture()
@@ -75,34 +158,65 @@ public class Compute : MonoBehaviour
         return tex;
     }
 
+    private void createTextures()
+    {
+        sourceTex = createTexture();
+        brightTex = createTexture();
+        blurrBuff = createTexture();
+        caResult = createTexture();
+        lfResult = createTexture();
+        lensDirt = createTexture();
+        Graphics.Blit(lensDirtTex, lensDirt);
+    }
+
+    private void setTextures()
+    {
+        setBloomTextures();
+        setCATextures();
+        setLensFlareTextures();
+    }
+
     private void setBloomUniforms()
     {
         bloom.SetFloat("threshold", bloomThreshold);
+    }
 
-        bloom.SetTexture(0, "source", sourceTex);
+    private void setBloomTextures()
+    {
+        bloom.SetTexture(0, "Source", sourceTex);
         bloom.SetTexture(0, "BrightSpots", brightTex);
 
+        bloom.SetTexture(1, "Source", sourceTex);
         bloom.SetTexture(1, "BrightSpots", brightTex);
-        bloom.SetTexture(1, "BlurrBuffer", blurrBuff);
-
-        bloom.SetTexture(2, "BrightSpots", brightTex);
-        bloom.SetTexture(2, "BlurrBuffer", blurrBuff);
-
-        bloom.SetTexture(3, "source", sourceTex);
-        bloom.SetTexture(3, "BrightSpots", brightTex);
     }
 
     private void setCAUniforms()
     {
         chromaticAberration.SetInt("amount", caAmount);
+    }
 
+    private void setCATextures()
+    {
         chromaticAberration.SetTexture(0, "Source", sourceTex);
         chromaticAberration.SetTexture(0, "Result", caResult);
     }
 
     private void setLensFlareUniforms()
     {
-        lensFlare.SetInt("ghostCount", 2);
-        lensFlare.SetFloat("ghostSpacing", 1);
+        lensFlare.SetInt("width", Screen.width);
+        lensFlare.SetInt("height", Screen.height);
+        lensFlare.SetInt("ghostCount", lfGhostCount);
+        lensFlare.SetFloat("ghostSpacing", lfGhostSpacing);
+        lensFlare.SetFloat("threshold", lfThreshold);
+        lensFlare.SetFloat("caStrength", lfCAStrength);
+    }
+
+    private void setLensFlareTextures()
+    {
+        lensFlare.SetTexture(0, "Source", sourceTex);
+        lensFlare.SetTexture(0, "Result", lfResult);
+        lensFlare.SetTexture(1, "lensDirt", lensDirt);
+        lensFlare.SetTexture(1, "Source", caResult);
+        lensFlare.SetTexture(1, "Result", lfResult);
     }
 }
---
+++ Assets/Shader/Blurr.shader
@@ -0,0 +1,142 @@
+Shader "Hidden/Blurr"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _horizontal ("Horizontal", int) = 1
+        _vignetteAmount ("Amount", float) = 1.0
+        _vignetteWidth ("Width", float) = 0.1
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            static const float weight[3] = { 0.2270270270, 0.3162162162, 0.0702702703 };
+            static const float offset[3] = { 0.0, 1.3846153846, 3.2307692308 };
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _MainTex;
+            float4 _MainTex_TexelSize;
+            int _horizontal;
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = tex2D(_MainTex, i.uv) * weight[0];
+                float2 texcoord = i.uv;
+
+                if (_horizontal == 1)
+                {
+                    for (int i = 1; i < 3; ++i)
+                    {
+                        float xOffset = offset[i] * _MainTex_TexelSize.x;
+                        col += tex2D(_MainTex, texcoord + float2(xOffset, 0)) * weight[i];
+                        col += tex2D(_MainTex, texcoord - float2(xOffset, 0)) * weight[i];
+                    }
+                }
+                else
+                {
+                    for (int i = 1; i < 3; ++i)
+                    {
+                        float yOffset = offset[i] * _MainTex_TexelSize.y;
+                        col += tex2D(_MainTex, texcoord + float2(0, yOffset)) * weight[i];
+                        col += tex2D(_MainTex, texcoord - float2(0, yOffset)) * weight[i];
+                    }
+                }
+                return col;
+            }
+            ENDCG
+        }
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _MainTex;
+            float _vignetteAmount;
+            float _vignetteWidth;
+
+            fixed4 getVignetteCol(float2 uv) 
+            {
+                fixed4 col = tex2D(_MainTex, uv);
+
+                float2 texcoord = abs(uv * 2 - 1);
+
+                fixed2 u = texcoord * _vignetteWidth;
+                u = 1 - (pow(smoothstep(u, 0, 1 - texcoord), 4) * _vignetteAmount);
+
+                col.rgb *= u.x * u.y;
+                return col;
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                float2 uv = i.uv * 2 - 1;
+
+                uv.x *= 1 + pow(abs(uv.y) / 8, 2);
+                uv.y *= 1 + pow(abs(uv.x) / 8, 2);
+
+                uv = uv / 2 + .5;
+
+                fixed4 col = fixed4(0, 0, 0, 1);
+
+                if (uv.x <= 1 && uv.x >= 0 && uv.y <= 1 && uv.y >= 0)
+                    col = getVignetteCol(uv);
+
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
